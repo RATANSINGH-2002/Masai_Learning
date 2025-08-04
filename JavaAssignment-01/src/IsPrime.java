073de9e8b50cb4f49232899a2cbd34d7a84592f1
Ratan Tejnarayan Singh <ratansgh21@gmail.com>
2025-08-04 18:18:12 +0530

    Assignment Done

--- JavaAssignment-01/src/IsPrime.java
+++ JavaAssignment-01/src/IsPrime.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class IsPrime {
+    public static void main(String[] args) {
+        for (int i = 2; i < 51; i++) {
+
+            boolean IsPrime = true;
+
+            for( int j = 2; j < i; j++){
+                if(i % j == 0){
+                    IsPrime = false;
+                    break;
+                }
+            }
+
+            if(IsPrime){
+                System.out.println(i);
+            }
+        }
+    }
 }
